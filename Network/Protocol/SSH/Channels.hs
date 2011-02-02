0b875b971baedef75dbae0e1e5ae322a7273c02b
Irene Knapp <irene.knapp@icloud.com>
2011-02-02 02:08:52 +0000

    Now the actual implementations are filled in for the three "meat" functions in Channels.hs.

--- Network/Protocol/SSH/Channels.hs
+++ Network/Protocol/SSH/Channels.hs
@@ -13,7 +13,9 @@ import Network.Protocol.SSH.Internal
 
 
 data ChannelOpen
-  = ChannelOpenX11 {
+  = ChannelOpenSession {
+    }
+  | ChannelOpenX11 {
       channelOpenOriginatorAddress :: String,
       channelOpenOriginatorPort :: Word32
     }
@@ -93,7 +95,58 @@ streamReadChannelOpenFields :: AbstractStream
                             -> String
                             -> IO (Maybe ChannelOpen)
 streamReadChannelOpenFields stream channelType = do
-  undefined
+  case channelType of
+    "session" -> do
+      return $ Just ChannelOpenSession { }
+    "x11" -> do
+      maybeOriginatorAddress <- streamReadString stream
+      maybeOriginatorPort <- streamReadWord32 stream
+      case maybeOriginatorPort of
+        Nothing -> return Nothing
+        Just _ -> return $ Just
+                         ChannelOpenX11 {
+                             channelOpenOriginatorAddress
+                               = fromJust maybeOriginatorAddress,
+                             channelOpenOriginatorPort
+                               = fromJust maybeOriginatorPort
+                           }
+    "forwarded-tcpip" -> do
+      maybeAddressConnected <- streamReadString stream
+      maybePortConnected <- streamReadWord32 stream
+      maybeOriginatorAddress <- streamReadString stream
+      maybeOriginatorPort <- streamReadWord32 stream
+      case maybeOriginatorPort of
+        Nothing -> return Nothing
+        Just _ -> return $ Just
+                         ChannelOpenForwardedTCPIP {
+                             channelOpenAddressConnected
+                               = fromJust maybeAddressConnected,
+                             channelOpenPortConnected
+                               = fromJust maybePortConnected,
+                             channelOpenOriginatorAddress
+                               = fromJust maybeOriginatorAddress,
+                             channelOpenOriginatorPort
+                               = fromJust maybeOriginatorPort
+                           }
+    "direct-tcpip" -> do
+      maybeAddressToConnect <- streamReadString stream
+      maybePortToConnect <- streamReadWord32 stream
+      maybeOriginatorAddress <- streamReadString stream
+      maybeOriginatorPort <- streamReadWord32 stream
+      case maybeOriginatorPort of
+        Nothing -> return Nothing
+        Just _ -> return $ Just
+                         ChannelOpenForwardedTCPIP {
+                             channelOpenAddressToConnect
+                               = fromJust maybeAddressToConnect,
+                             channelOpenPortToConnect
+                               = fromJust maybePortToConnect,
+                             channelOpenOriginatorAddress
+                               = fromJust maybeOriginatorAddress,
+                             channelOpenOriginatorPort
+                               = fromJust maybeOriginatorPort
+                           }
+    _ -> error "Unknown SSH channel type."
 
 
 streamReadChannelOpenConfirmationFields :: AbstractStream
@@ -101,11 +154,158 @@ streamReadChannelOpenConfirmationFields :: AbstractStream
                                         -> ChannelOpen
                                         -> IO (Maybe ChannelOpenConfirmation)
 streamReadChannelOpenConfirmationFields stream channelType channelOpen = do
-  undefined
+  case channelType of
+    "session" -> do
+      return $ Just ChannelOpenConfirmationNone { }
+    "x11" -> do
+      return $ Just ChannelOpenConfirmationNone { }
+    "forwarded-tcpip" -> do
+      return $ Just ChannelOpenConfirmationNone { }
+    "direct-tcpip" -> do
+      return $ Just ChannelOpenConfirmationNone { }
+    _ -> error "Unknown SSH channel type."
 
 
 streamReadChannelRequestFields :: AbstractStream
                                -> String
                                -> IO (Maybe ChannelRequest)
 streamReadChannelRequestFields stream requestType = do
-  undefined
+  case requestType of
+    "pty-req" -> do
+      maybeTerminalType <- streamReadString stream
+      maybeTerminalWidthCharacters <- streamReadWord32 stream
+      maybeTerminalHeightCharacters <- streamReadWord32 stream
+      maybeTerminalWidthPixels <- streamReadWord32 stream
+      maybeTerminalHeightPixels <- streamReadWord32 stream
+      maybeTerminalModes <- streamReadString stream
+      case maybeTerminalModes of
+        Nothing -> return Nothing
+        Just _ -> return $ Just
+                         ChannelRequestPTY {
+                             channelRequestTerminalType
+                               = fromJust maybeTerminalType,
+                             channelRequestTerminalWidthCharacters
+                               = fromJust maybeTerminalWidthCharacters,
+                             channelRequestTerminalHeightCharacters
+                               = fromJust maybeTerminalHeightCharacters,
+                             channelRequestTerminalWidthPixels
+                               = fromJust maybeTerminalWidthPixels,
+                             channelRequestTerminalHeightPixels
+                               = fromJust maybeTerminalHeightPixels,
+                             channelRequestTerminalModes
+                               = fromJust maybeTerminalModes
+                           }
+    "x11-req" -> do
+      maybeSingleConnection <- streamReadBoolean stream
+      maybeX11AuthenticationProtocol <- streamReadString stream
+      maybeX11AuthenticationCookie <- streamReadString stream
+      maybeX11ScreenNumber <- streamReadString stream
+      case maybeX11ScreenNumber of
+        Nothing -> return Nothing
+        Just _ -> return $ Just
+                         ChannelRequestX11 {
+                             channelRequestSingleConnection
+                               = fromJust maybeSingleConnection,
+                             channelRequestX11AuthenticationProtocol
+                               = fromJust maybeX11AuthenticationProtocol,
+                             channelRequestX11AuthenticationCookie
+                               = fromJust maybeX11AuthenticationCookie,
+                             channelRequestX11ScreenNumber
+                               = fromJust maybeX11ScreenNumber
+                           }
+    "env" -> do
+      mabyeVariableName <- streamReadString stream
+      maybeVariableValue <- streamReadString stream
+      case maybeVariableValue of
+        Nothing -> return Nothing
+        Just _ -> return $ Just
+                         ChannelRequestEnvironment {
+                             channelRequestVariableName
+                               = fromJust maybeVariableName,
+                             channelRequestVariableValue
+                               = fromJust maybeVariableValue
+                           }
+    "shell" -> do
+      return $ Just ChannelRequestShell { }
+    "exec" -> do
+      maybeCommand <- streamReadString stream
+      case maybeCommand of
+        Nothing -> return Nothing
+        Just _ -> return $ Just
+                         ChannelRequestExecute {
+                             channelRequestCommand
+                               = fromJust maybeCommand
+                           }
+    "subsystem" -> do
+      maybeSubsystemName <- streamReadString stream
+      case maybeSubsystemName of
+        Nothing -> return Nothing
+        Just _ -> return $ Just
+                         ChannelRequestSubsystem {
+                             channelRequestSubsystemName
+                               = fromJust maybeSubsystemName
+                           }
+    "window-change" -> do
+      maybeTerminalWidthCharacters <- streamReadWord32 stream
+      maybeTerminalHeightCharacters <- streamReadWord32 stream
+      maybeTerminalWidthPixels <- streamReadWord32 stream
+      maybeTerminalHeightPixels <- streamReadWord32 stream
+      case maybeTerminalHeightPixels of
+        Nothing -> return Nothing
+        Just _ -> return $ Just
+                         ChannelRequestWindowChange {
+                             channelRequestTerminalWidthCharacters
+                               = fromJust maybeTerminalWidthCharacters,
+                             channelRequestTerminalHeightCharacters
+                               = fromJust maybeTerminalHeightCharacters,
+                             channelRequestTerminalWidthPixels
+                               = fromJust maybeTerminalWidthPixels,
+                             channelRequestTerminalHeightPixels
+                               = fromJust maybeTerminalHeightPixels
+                           }
+    "xon-xoff" -> do
+      maybeClientCanDo <- streamReadBoolean stream
+      case maybeClientCanDo of
+        Nothing -> return Nothing
+        Just _ -> return $ Just
+                         ChannelRequestXonXoff {
+                             channelRequestClientCanDo
+                               = fromJust maybeClientCanDo
+                           }
+    "signal" -> do
+      maybeSignalName <- streamReadString stream
+      case maybeSignalName of
+        Nothing -> return Nothing
+        Just _ -> return $ Just
+                         ChannelRequestSignal {
+                             channelRequestSignal
+                               = fromJust maybeSignalName
+                           }
+    "exit-status" -> do
+      maybeExitStatus <- streamReadWord32 stream
+      case maybeExitStatus of
+        Nothing -> return Nothing
+        Just _ -> return $ Just
+                         ChannelRequestExitStatus {
+                             channelRequestExitStatus
+                               = fromJust maybeExitStatus
+                           }
+    "exit-signal" -> do
+      maybeSignalName <- streamReadString stream
+      maybeCoreDumped <- streamReadBoolean stream
+      maybeMessage <- streamReadString stream
+      maybeLanguageTag <- streamReadString stream
+      case maybeLanguageTag of
+        Nothing -> return Nothing
+        Just _ -> return $ Just
+                         ChannelRequestExitSignal {
+                             channelRequestSignalName
+                               = fromJust maybeSignalName,
+                             channelRequestCoreDumped
+                               = fromJust maybeCoreDumped,
+                             channelRequestMessage
+                               = fromJust maybeMessage,
+                             channelRequestLanguageTag
+                               = fromJust maybeLanguageTag
+                           }
+    _ -> error "Unknown SSH channel-request type."
